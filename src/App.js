9014903e4f96983976bbdbe750d1c4070aeab85c
DiegoRz <adamantwolf@hotmail.com>
2023-08-15 10:36:04 -0500

    Added i18n and routes to structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Route, Routes, Link } from 'react-router-dom'
+import React from 'react'
+
+
 function App() {
   return (
     <div className="App">
